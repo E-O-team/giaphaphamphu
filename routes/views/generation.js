70458ac4b63884475eaa1a19fce4418d70cb28c4
NeoTheSecond <quangman1404@gmail.com>
2018-06-11 11:21:43 +0700

    added generation filter, pagination for people, also fixed minor bugs

--- routes/views/generation.js
+++ routes/views/generation.js
@@ -20,28 +20,20 @@ exports = module.exports = function (req, res) {
 
         var q = keystone.list('Person').paginate({
             filters: {
-                slug: { $regex: locals.filters.q , $options: 'i'},
+                generation: locals.filters.q
             },
         })
         q.exec(function (err, results) {
+            if(err) {
+                console.log('Error is: ' + err);
+                return;
+            }
             locals.data.people = results;
-            console.log(locals.data.people);
             next(err);
         });
 
     });
 
-    // view.on('init', function (next) {
-    //   var q = keystone.list('Person').model.find().where('fullName', '$regex: locals.filters.q');
-    //   q.exec(function (err, results) {
-    //       console.log(results);
-    //       locals.data.people = results;
-    //       next(err);
-    //   });
-    //
-    // });
-
-
     // Render the view
-    view.render('search');
+    view.render('generation');
 };
