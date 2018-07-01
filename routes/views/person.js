f9e9a3968912288ba07aaa22584f89f3fccbb198
NeoTheSecond <quangman1404@gmail.com>
2018-07-01 10:00:40 +0700

    using og view

--- routes/views/person.js
+++ routes/views/person.js
@@ -18,55 +18,55 @@ exports = module.exports = function(req, res) {
 		})
 	}
 	//OG
-    // function getFamilyTree(person){
-    //     getChildren(person, function(children){
-    //         person.children=children;
-    //         for (var i=0;i<person.children.length;i++) {
-	// 			!function outer(i){
-	// 				if (isNotEmpty(person.children[i])){
-	// 					getChildren(person.children[i],function(children){
-	// 							person.children[i].children=children;
-	// 							for (var j=0;j<person.children[i].children.length;j++){
-	// 								!function outer(j){
-	// 									if (isNotEmpty(person.children[i].children[j])){
-	// 										getChildren(person.children[i].children[j],function(children){
-	// 												person.children[i].children[j].children=children;
-	// 												for (var k=0;k<person.children[i].children[j].children.length;k++){
-	// 													!function outer(k){
-	// 														if (isNotEmpty(person.children[i].children[j].children[k])){
-	// 															getChildren(person.children[i].children[j].children[k],function(children){
-	// 																person.children[i].children[j].children[k].children=children;
-	// 																// console.log(person.children[i].children[j].children[k].children);
-	// 															})
-	// 														}
-	// 													}(k);
-	// 												}
-	// 										})
-	// 									}
-	//
-	// 								}(j);
-	// 							}
-	// 	                });
-	// 				}
-	//
-	// 			}(i);
-    //         }
-    //     })
-    // }
+    function getFamilyTree(person){
+        getChildren(person, function(children){
+            person.children=children;
+            for (var i=0;i<person.children.length;i++) {
+				!function outer(i){
+					if (isNotEmpty(person.children[i])){
+						getChildren(person.children[i],function(children){
+								person.children[i].children=children;
+								for (var j=0;j<person.children[i].children.length;j++){
+									!function outer(j){
+										if (isNotEmpty(person.children[i].children[j])){
+											getChildren(person.children[i].children[j],function(children){
+													person.children[i].children[j].children=children;
+													for (var k=0;k<person.children[i].children[j].children.length;k++){
+														!function outer(k){
+															if (isNotEmpty(person.children[i].children[j].children[k])){
+																getChildren(person.children[i].children[j].children[k],function(children){
+																	person.children[i].children[j].children[k].children=children;
+																	// console.log(person.children[i].children[j].children[k].children);
+																})
+															}
+														}(k);
+													}
+											})
+										}
+
+									}(j);
+								}
+		                });
+					}
+
+				}(i);
+            }
+        })
+    }
 
 	//Stack's
-	function getFamilyTree ( person ) {
-		if (isNotEmpty(person)){
-			getChildren( person, function( children ) {
-		        person.children = children;
-		        for ( var i = 0; i < person.children.length;i++ ) {
-					if (isNotEmpty(person.children[i])){
-						getFamilyTree( person.children[ i ] );
-					}
-		        }
-		    })
-		}
-	}
+	// function getFamilyTree ( person ) {
+	// 	if (isNotEmpty(person)){
+	// 		getChildren( person, function( children ) {
+	// 	        person.children = children;
+	// 	        for ( var i = 0; i < person.children.length;i++ ) {
+	// 				if (isNotEmpty(person.children[i])){
+	// 					getFamilyTree( person.children[ i ] );
+	// 				}
+	// 	        }
+	// 	    })
+	// 	}
+	// }
 
 function isNotEmpty(obj) {
 	for(var key in obj) {
